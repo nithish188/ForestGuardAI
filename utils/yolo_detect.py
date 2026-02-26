bce2473c7c2bcb7c60f866f458f55f01a8fa69dc
Nithish <mailtonithishkumar4@gmail.com>
2026-02-27 01:36:52 +0530

    Update yolo_detect.py

--- utils/yolo_detect.py
+++ utils/yolo_detect.py
@@ -1,9 +1,22 @@
 from ultralytics import YOLO
-import cv2
 
 model = YOLO("yolov8n.pt")
 
+THREAT_CLASSES = ["person", "car", "motorcycle", "bus", "truck"]
+
 def detect_intrusion(image_path):
+
     results = model(image_path)
-    result_img = results[0].plot()
-    return result_img
+
+    detected_classes = []
+
+    for box in results[0].boxes:
+        cls_id = int(box.cls[0])
+        class_name = model.names[cls_id]
+        detected_classes.append(class_name)
+
+    intrusion = any(cls in THREAT_CLASSES for cls in detected_classes)
+
+    plotted = results[0].plot()
+
+    return plotted, intrusion, detected_classes
